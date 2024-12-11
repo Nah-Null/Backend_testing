8b7494367349b4d68d85f3da0c518268873d7e72
Pu <pgthman@gmail.com>
2024-12-11 11:36:30 +0700

    01

---
+++ controllers/storeUser.js
@@ -0,0 +1,17 @@
+// ป้องกันการไม่ใส่ข้อมูล
+
+const User = require('../models/user')
+
+module.exports = (req, res) => {
+    User.create(req.body).then(() => {
+        console.log("User register successfully!")
+        rea.redirect('/')
+    }).catch((error) => {
+        if (error){
+            const validationErrors = Object.keys(error.errors).map(key => error.errors[key].message)
+            req.flash('validationErrors',validationErrors)
+            req.flash('data', req.body)
+            return res.redirect('/register')
+        }
+    })
+}
---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const expressSession = require('express-session')
+const flash = require('connect-flash')
+
+// Connect MongoDB
+mongoose.connect('mongodb+srv://noobboy:Dragon_Killer@cluster0.kuhyr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+    useNewUrlParser: true
+})
+
+const indexcontro = require('./controllers/indexcontro') //Objectเก็บหน้าแรก
+const logincontro = require('./controllers/logincontro') //Objectเก็บหน้า log-in
+const registercontro = require('./controllers/registercontro') //Objectเก็บหน้า register
+const storecontro = require('./controllers/storeUser') //Objectเก็บการป้องกันการไม่ใส่ข้อมูล
+
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(flash())
+app.use(expressSession({
+    secret:"node secret"
+}))
+app.set('view engine','ejs')
+
+
+
+app.get('/',indexcontro) //เรียกใช้หน้า register
+app.get('/login',logincontro) //เรียกใช้หน้า register
+app.get('/register',registercontro) //เรียกใช้หน้า register
+app.post('/user/register',storecontro)//เรียกใช้การป้องกันการไม่ใส่ข้อมูล
+
+
+app.listen(4000,() => {
+    console.log("\n running port 4000 pass \n")
+})
